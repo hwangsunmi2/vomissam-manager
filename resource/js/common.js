3c5c7db9a514e4c256ee4ebcee241b0a83040bd0
red <red@wordncode.com>
2022-11-17 11:39:51 +0900

    기존 폴더 붙여넣기

---
+++ resource/js/common.js
@@ -0,0 +1,66 @@
+
+
+$(function(){
+    // 
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var quickBarHeight = $('.quick-menu').outerHeight();
+
+    $(window).scroll(function(event){
+        didScroll = true;
+    });
+
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 100);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+        
+        // Make sure they scroll more than delta
+        if(Math.abs(lastScrollTop - st) <= delta)
+            return;
+        if (st > lastScrollTop && st > quickBarHeight){
+            // Scroll Down
+            $('.quick-menu').removeClass('is-fix').addClass('is-show').css("bottom","-32.6rem");
+
+            if(st == $(document).height() - $(window).height()){
+                $('.quick-menu').addClass('is-fix').css("bottom","0");
+                
+            } else {
+                //아닐때 이벤트
+            }
+        } else {
+            // Scroll Up
+            if(st + $(window).height() < $(document).height()) {
+                $('.quick-menu').removeClass('is-show').css("bottom","0rem");
+                
+            }
+        }
+        
+        lastScrollTop = st;
+    }
+
+    
+
+    // $(window).scroll(function(){
+    //     var scrT = $(window).scrollTop();
+    //     //console.log(scrT); //스크롤 값 확인용
+    //     if(scrT == $(document).height() - $(window).height()){
+    //         $('.quick-menu').addClass('is-show');
+    //     } else {
+    //         //아닐때 이벤트
+    //     }
+    //   })
+
+
+    $(".quick-menu__button").click(function(){
+        $(this).addClass("is-active");
+        $(this).siblings().removeClass("is-active");
+    })
+
+})
---
+++ resource/js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+  introSlide();
+  videoArea();
+  lookServiceSwiper();
+  carerList();
+
+});
+
+function introSlide() {
+  var introSlide = new Swiper(".intro-slide.swiper-container", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+}
+
+function videoArea() {
+  var mainswiper = new Swiper(".video-area__inner.swiper-container", {});
+}
+
+function lookServiceSwiper() {
+  var mainswiper = new Swiper(".look-service__inner.swiper-container", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+  });
+}
+
+//보미쌤 지원 현황
+function carerList() {
+  $(".carer-button__detail").click(function(){
+    $(this).hide();
+    $(this).parent(".carer-buttons").children(".carer-button__wish, .carer-button__choice").show();
+    $(this).closest(".carer-list").toggleClass("is-show");
+  })
+}
